88afd72e744782d2b15e4065272d4050faba6a74
kamrul <cdkamrul9@gmail.com>
2026-06-21 23:25:33 +0600

    128-optimal one and previous error fix for edge case

--- Array/128_longest_consecutive_sequence.py
+++ Array/128_longest_consecutive_sequence.py
@@ -1,8 +1,11 @@
 def longest_consecutive_sequence(nums):
+    if not nums:
+        return 0
+
     nums.sort()
 
     count = 1
-    result = 0
+    result = 1
 
     for i in range(1, len(nums)):
         if nums[i] == nums[i-1]:
@@ -10,8 +13,27 @@ def longest_consecutive_sequence(nums):
         elif nums[i] == nums[i-1] + 1:
             count += 1
         else:
-            result = max(count, result)
-            count = 1 
-    return result
+            result = max(result, count)
+            count = 1
 
-print(longest_consecutive_sequence([100,4,200,1,3,2]))
+    return max(result, count) 
+#instead of returning result we return max because if the array is alrewady the longest sequence it wont give correct result
+# this one O(nlogn) nad O(1)
+
+# we can do it in O(n), but space will also be O(n)
+
+# Optimal solution
+def longest_consecutive_sequence(nums):
+    nums_set = set(nums)
+    longest = 0
+
+    for num in nums_set:
+        if num - 1 not in nums_set:  # start of sequence
+            length = 1
+
+            while num + length in nums_set:
+                length += 1
+
+            longest = max(longest, length)
+
+    return longest
