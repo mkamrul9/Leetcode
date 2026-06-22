961d85eb0d5c1040e2dedbe0b898b22b8a5d57fb
kamrul <cdkamrul9@gmail.com>
2026-06-22 10:53:52 +0600

    238. Product of Array Except Self

---
+++ Array/238_product_of_array_except_self.py
@@ -0,0 +1,17 @@
+# Optimal approach
+#  It multiplies all num before i which is prefix array with all number after i which is suffix array
+def productExceptSelf(nums):
+    n = len(nums)
+    res = [1] * n
+
+    prefix = 1
+    for i in range(n):
+        res[i] = prefix
+        prefix *= nums[i]
+
+    suffix = 1
+    for i in range(n - 1, -1, -1):
+        res[i] *= suffix
+        suffix *= nums[i]
+
+    return res
