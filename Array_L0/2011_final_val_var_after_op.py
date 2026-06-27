6c530b06aed37f934ed1232b5d358d50d8f61cac
kamrul <cdkamrul9@gmail.com>
2026-06-27 20:22:53 +0600

    977_squares_of_sorted_arr.py

--- Array_L0/2011_final_val_var_after_op.py
+++ Array_L0/2011_final_val_var_after_op.py
@@ -9,3 +9,15 @@ class Solution:
                 x -= 1
 
         return x
+    
+class Solution:
+    def finalValueAfterOperations(operations):
+        x = 0
+
+        for op in operations:
+            if '+' in op:
+                x += 1
+            else:
+                x -= 1
+
+        return x
